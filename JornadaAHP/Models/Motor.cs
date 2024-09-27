efa62b6466d29badb6f9651908c5567cb4662c76
Your Name <you@example.com>
2024-09-27 20:56:29 -0300

    finalizado primeira parte

---
+++ JornadaAHP/Models/Motor.cs
@@ -0,0 +1,11 @@
+﻿namespace JornadaAHP.Models
+{
+    public class Motor
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Prazo { get; set; } // em dias
+        public int Velocidade { get; set; } // em RPM
+        public int Peso { get; set; } // em Kg
+    }
+}
