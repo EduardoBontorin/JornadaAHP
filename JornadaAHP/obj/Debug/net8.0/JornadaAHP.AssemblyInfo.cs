bdd50563e2a13f14dd9c310c70ee97807befe9c9
Your Name <you@example.com>
2024-11-22 21:48:55 -0300

    finalizado essa bosta

--- JornadaAHP/obj/Debug/net8.0/JornadaAHP.AssemblyInfo.cs
+++ JornadaAHP/obj/Debug/net8.0/JornadaAHP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JornadaAHP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7381d678ec72979fef133d80087f256094076e74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc022dc07db86488caf858981f98759734eabbd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("JornadaAHP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JornadaAHP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
